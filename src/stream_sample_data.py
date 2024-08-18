21f57e729aac9cbfbc02035f514d2c241866451d
adisaljusi (アディス) <adis.aljusi@hotmail.com>
2024-08-18 22:01:54 +0200

    use kafka-python-ng instead as pip package fails

--- src/stream_sample_data.py
+++ src/stream_sample_data.py
@@ -2,6 +2,10 @@
 
 from faker import Faker
 import time
+import json
+from kafka import KafkaProducer
+import logging
+
 
 def create_sample_date(faker: Faker, id: int):
     return {
@@ -12,10 +16,18 @@ def create_sample_date(faker: Faker, id: int):
     }
 
 
-# def
+def send_to_kafka(producer: KafkaProducer, topic_name: str, data):
+    try:
+        producer.send(topic_name, json.dumps(data))
+    except Exception as e:
+        logging.error(f"Error while sending data to topic {topic_name}")
+        logging.error(str(e))
+
 
 if __name__ == "__main__":
     faker = Faker()
+    producer = KafkaProducer(bootstrap_servers=["localhost:29092"])
+
     for i in range(0, 10):
-        print(create_sample_date(faker, i))
+        send_to_kafka(producer, "user_entry", (create_sample_date(faker, i)))
         time.sleep(5)
